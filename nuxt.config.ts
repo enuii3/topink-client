7b96927a452b37211dc167b0a621bb36a7fc24f9
enuii3 <b.nxxxi551@gmail.com>
2023-04-06 00:50:09 +0900

    初期設定関連

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      title: "Top ink",
+    },
+  },
+  imports: {
+    dirs: ["features"],
+  },
+  // css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+});
