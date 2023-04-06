2b7c746f11031d811a870676e83a91e1af77f4c2
enuii3 <b.nxxxi551@gmail.com>
2023-04-07 01:24:13 +0900

    indexの作成

---
+++ features/Main/TopCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <v-img src="topink.jpg">
+      <strong class="top-message">『ミライへ繋げる、まちづくり。』</strong>
+    </v-img>
+  </div>
+</template>
+
+<style scoped>
+@media screen and (max-width: 600px) {
+  .top-message {
+    font-size: 20px;
+  }
+}
+
+/* 601px以上の画面には16pxのフォントサイズを指定 */
+@media screen and (min-width: 601px) {
+  .top-message {
+    font-size: 40px;
+  }
+}
+
+.top-message {
+  position: absolute;
+  bottom: 20%;
+  left: 4%;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Header } from "~~/features/Header";
+import { Footer } from "~~/features/Footer";
+</script>
+
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <slot />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<style scoped></style>
