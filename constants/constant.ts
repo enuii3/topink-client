ef71b4e836ea0342ea15d09bcf5c03dd5b26f7f2
enuii3 <b.nxxxi551@gmail.com>
2023-04-30 12:36:22 +0900

    constantsにtypeを追加し、内容の修正

--- constants/constant.ts
+++ constants/constant.ts
@@ -1,4 +1,6 @@
-export const topInkColors = [
+import { HeaderMenu, TopInkColor, TopInkCardIcon } from "~~/types/type";
+
+export const topInkColors: TopInkColor[] = [
   "#009900",
   "#ffa500",
   "#0000ff",
@@ -6,29 +8,51 @@ export const topInkColors = [
   "#ff0000",
   "#ffd700",
 ];
-export const headerMenu = [
+
+const topInkCardIcon: TopInkCardIcon[] = [
+  "topink-t.png",
+  "topink-o.png",
+  "topink-p.png",
+  "topink-i.png",
+  "topink-n.png",
+  "topink-k.png",
+];
+
+export const headerMenus: HeaderMenu[] = [
   {
     title: "mission",
     text: "ミッション",
+    icon: topInkCardIcon[0],
+    color: topInkColors[0],
   },
   {
     title: "businesses",
     text: "事業紹介",
+    icon: topInkCardIcon[1],
+    color: topInkColors[1],
   },
   {
     title: "aboutUs",
     text: "私たち",
+    icon: topInkCardIcon[2],
+    color: topInkColors[2],
   },
   {
     title: "office",
     text: "会社情報",
+    icon: topInkCardIcon[3],
+    color: topInkColors[3],
   },
   {
     title: "recruit",
     text: "採用情報",
+    icon: topInkCardIcon[4],
+    color: topInkColors[4],
   },
   {
     title: "contact",
     text: "お問合せ",
+    icon: topInkCardIcon[5],
+    color: topInkColors[5],
   },
 ];
