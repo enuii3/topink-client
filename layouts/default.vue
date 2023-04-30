81c515bb3ea5ec0f4d4c1f136d75863e695b949b
enuii3 <b.nxxxi551@gmail.com>
2023-04-30 12:18:52 +0900

    v-mainの中央の変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,23 +6,18 @@ import { Footer } from "~~/features/Footer";
 <template>
   <v-app>
     <Header />
-    <v-main class="vmain-container">
-      <div class="vmain">
-        <slot />
-      </div>
+    <v-main class="vmain">
+      <slot />
     </v-main>
     <Footer />
   </v-app>
 </template>
 
 <style scoped>
-.vmain-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 .vmain {
   max-width: 1200px;
+  margin: 0 auto;
+  width: 100%;
 }
 @media screen and (max-width: 600px) {
   .vmain {
