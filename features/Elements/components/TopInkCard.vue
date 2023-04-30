409ce657ab04b365624cd843ef3f261875463843
enuii3 <b.nxxxi551@gmail.com>
2023-04-30 15:21:16 +0900

    TopInkCardのデザイン変更

--- features/Elements/components/TopInkCard.vue
+++ features/Elements/components/TopInkCard.vue
@@ -1,31 +1,84 @@
 <script setup lang="ts">
-import { topInkColors } from "~~/constants/constant";
-import NavBarMenuTextTitle from "./NavBarMenuTextTitle.vue";
+import {
+  HeaderMenu,
+  HeaderMenuText,
+  HeaderMenuTitle,
+  TopInkCardIcon,
+  TopInkColor,
+} from "~~/types/type";
 
-interface Props {
-  title: string;
-  menuIndexNumber: number;
+interface Props extends HeaderMenu {
+  title: HeaderMenuTitle;
+  text: HeaderMenuText;
+  icon: TopInkCardIcon;
+  color: TopInkColor;
+  positionRight?: string;
+  positionBottom?: string;
+  bottomTitleWidth: string;
 }
-const props = defineProps<Props>();
+
+defineProps<Props>();
 </script>
 
 <template>
-  <div
-    class="ma-10 pa-10"
-    data-aos="fade-in"
-    data-aos-offset="200"
-    data-aos-delay="200"
-    data-aos-duration="2000"
-    :style="{
-      'border-radius': '20px',
-      'border-width': '2px',
-      'border-style': 'solid',
-      'border-color': topInkColors[menuIndexNumber],
-    }"
-  >
-    <NavBarMenuTextTitle variant="main" :menu-index-number="menuIndexNumber">
-      {{ title }}
-    </NavBarMenuTextTitle>
-    <slot />
+  <!-- <div :id="title" class="ma-5" data-aos="fade-in" data-aos-offset="200" data-aos-delay="200" data-aos-duration="2000" style="position: static" > -->
+  <div class="py-10">
+    <div style="position: relative">
+      <div :id="title" class="mx-5 mx-sm-7 h-100">
+        <div class="title-bar">
+          <img class="prepend-icon" :src="icon" :alt="title" />
+          <h2 class="title">
+            {{ text }}
+          </h2>
+        </div>
+        <slot />
+      </div>
+
+      <div
+        class="background-text"
+        :style="{
+          color: color,
+          width: bottomTitleWidth,
+          position: 'absolute',
+          bottom: positionBottom,
+          right: 0,
+        }"
+      >
+        {{ title }}
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.title-bar {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+}
+
+.prepend-icon {
+  margin-right: 10px;
+  width: 40px;
+  height: 40px;
+}
+
+.title {
+  font-family: "YuGothic";
+}
+
+.background-text {
+  overflow: hidden;
+  font-family: "Impact";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 100px;
+  line-height: 122px;
+  display: flex;
+  align-items: center;
+  text-transform: capitalize;
+
+  opacity: 0.2;
+  mix-blend-mode: darken;
+}
+</style>
