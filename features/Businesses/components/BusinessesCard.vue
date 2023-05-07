6491540638e4ed40b3cfe5a1faf3c24e7bd965fa
enuii3 <b.nxxxi551@gmail.com>
2023-05-07 22:39:23 +0900

    TopInkItemCardの作成とそれに伴うrecruit businessカードの編集

--- features/Businesses/components/BusinessesCard.vue
+++ features/Businesses/components/BusinessesCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { TopInkCard } from "~~/features/Elements";
+import { TopInkCard, TopInkItemCard } from "~~/features/Elements";
 import { HeaderMenu } from "~~/types/type";
-import BusinessCard from "./BusinessCard.vue";
 
 interface Props {
   headerMenu: HeaderMenu;
@@ -15,30 +14,35 @@ const businesses = [
     title: "リフォーム事業",
     description:
       "私たちは、お客様のご要望に合わせたプランをご提案し、お客様が理想とする住まいを実現するお手伝いをしています。住宅のリフォームは、お客様のライフスタイルやライフイベントに合わせて変化していくものです。",
+    image: "topink-top.jpg",
   },
   {
     icon: "mdi-wall",
     title: "外壁事業",
     description:
       "外壁は、建物の顔であり、建物を守る重要な要素の1つです。私たちは、お客様のご要望に合わせて、塗装色や素材を提案し、美しく長持ちする外壁を実現しています。",
+    image: "topink-top.jpg",
   },
   {
     icon: "mdi-compost",
     title: "省エネ事業",
     description:
       "私たちは、地球環境に配慮しながら、お客様の省エネニーズに応えるために、LED照明や断熱材の取り付け、太陽光発電システムの設置などを提供しています。",
+    image: "topink-top.jpg",
   },
   {
     icon: "mdi-poll",
     title: "SNSマーケティング",
     description:
       "私たちは、Facebook、Instagram、Twitter、LinkedInなどの主要なソーシャルメディアプラットフォームを活用して、クライアントのビジネスの知名度を高め、ブランドを強化し、収益を増やすことをサポートします。",
+    image: "topink-top.jpg",
   },
   {
     icon: "mdi-movie-filter",
     title: "動画プロモーション事業",
     description:
       "当社は、映像制作に特化したプロフェッショナルチームを擁しており、企業のブランディングや商品の販促に最適な動画を制作しています。 企画・撮影・編集までの全ての工程を一貫して自社で行うことで高品質かつスピーディーに対応します。",
+    image: "topink-top.jpg",
   },
 ];
 </script>
@@ -67,10 +71,11 @@ const businesses = [
           data-aos-delay="100"
           data-aos-duration="1000"
         >
-          <BusinessCard
+          <TopInkItemCard
             :icon="business.icon"
             :title="business.title"
             :description="business.description"
+            :image="business.image"
             :color="headerMenu.color"
           />
         </div>
--- features/Elements/index.ts
+++ features/Elements/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBarMenuTextTitle } from "./components/NavBarMenuTextTitle.vue";
 export { default as TopInkCard } from "./components/TopInkCard.vue";
+export { default as TopInkItemCard } from "./components/TopInkItemCard.vue";
 export { default as TopInkListTemplate } from "./components/TopInkListTemplate.vue";
