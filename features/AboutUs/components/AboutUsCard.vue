4ba4e44a8ddc511f49a5c1791d53c08217bbd135
enuii3 <b.nxxxi551@gmail.com>
2023-06-08 23:33:48 +0900

    About asの作成

--- features/AboutUs/components/AboutUsCard.vue
+++ features/AboutUs/components/AboutUsCard.vue
@@ -1,25 +1,48 @@
 <script setup lang="ts">
 import { TopInkCard } from "~~/features/Elements";
 import { HeaderMenu } from "~~/types/type";
+import { Staff } from "~~/types/type";
+import StaffCard from "./StaffCard.vue";
 
 interface Props {
   headerMenu: HeaderMenu;
 }
 
 defineProps<Props>();
+
+const staffList: Staff[] = [
+  {
+    name: "田島 憲人",
+    image: "default.png",
+    directorJob: "代表取締役 ",
+    directorOffice: "本社支店",
+    message:
+      "お客様の夢や目標を実現するために、最高品質の商品とサービスを提供することに全力を注いでいます。 私たちは常にお客様の声に耳を傾け、お客様のニーズや要望に真摯に向き合います。お客様の満足度を最優先に考え、信頼関係を築くことを大切にしています。私たちの仕事は単なるビジネスではなく、お客様とのパートナーシップを築きながら共に成長していくことです。 私たちは社会的責任も重要視しています。",
+  },
+  {
+    name: "酒井 貴寿",
+    image: "default.png",
+    directorJob: "取締役",
+    directorOffice: "神奈川支店",
+    message:
+      "お客様の夢や目標を実現するために、最高品質の商品とサービスを提供することに全力を注いでいます。 私たちは常にお客様の声に耳を傾け、お客様のニーズや要望に真摯に向き合います。お客様の満足度を最優先に考え、信頼関係を築くことを大切にしています。私たちの仕事は単なるビジネスではなく、お客様とのパートナーシップを築きながら共に成長していくことです。 私たちは社会的責任も重要視しています。",
+  },
+];
 </script>
 
 <template>
   <TopInkCard
     position-bottom="-50px"
     bottom-title-width="320px"
-    bottom-title-opacity="0.2"
+    background-color="rgba(209, 220, 240)"
     :text="headerMenu.text"
     :title="headerMenu.title"
     :color="headerMenu.color"
     :icon="headerMenu.icon"
   >
-    <div>私たちについて</div>
+    <div v-for="(staff, index) in staffList" :key="index">
+      <StaffCard :color="headerMenu.color" :staff="staff" />
+    </div>
   </TopInkCard>
 </template>
 
---
+++ features/AboutUs/components/StaffCard.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { Staff } from "~~/types/type";
+
+interface Props {
+  color: string;
+  staff: Staff;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="outline pa-1 pa-sm-6" style="z-index: 1">
+    <v-img
+      class="staff-img"
+      :src="staff.image"
+      :alt="staff.name"
+      aspect-ratio="16/9"
+    />
+
+    <div class="card-value">
+      <div
+        class="d-flex align-end"
+        :style="{
+          'border-bottom-width': '10px',
+          'border-bottom-color': 'white',
+        }"
+        data-aos="fade-in"
+        data-aos-offset="200"
+        data-aos-delay="200"
+        data-aos-duration="2000"
+      >
+        <h1 class="name">{{ staff.name }}</h1>
+        <h3 class="director job">{{ staff.directorJob }}</h3>
+        <h3 class="director office">{{ staff.directorOffice }}</h3>
+      </div>
+
+      <div class="message-box pa-sm-4">
+        <span
+          class="message"
+          data-aos="fade-in"
+          data-aos-offset="300"
+          data-aos-delay="300"
+          data-aos-duration="3000"
+          >{{ staff.message }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.v-img {
+  width: 200px;
+}
+
+.card-value {
+  padding: 200px 10px 10px 10px;
+}
+.outline {
+  position: relative;
+  margin-top: 20px;
+  border-radius: 8px;
+  border: solid 10px white;
+}
+
+.staff-img {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  opacity: 0.3;
+}
+
+.director {
+  margin-left: 20px;
+}
+
+.message-box {
+  z-index: 1;
+}
+
+.message {
+  letter-spacing: 2px;
+  line-height: 30px;
+  font-family: "YuGothic";
+  font-style: normal;
+  letter-spacing: 5px;
+  font-weight: 500;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+
+  color: #373737;
+}
+.director {
+  margin-left: 10px;
+}
+
+@media screen and (max-width: 600px) {
+  .office {
+    display: none;
+  }
+}
+</style>
